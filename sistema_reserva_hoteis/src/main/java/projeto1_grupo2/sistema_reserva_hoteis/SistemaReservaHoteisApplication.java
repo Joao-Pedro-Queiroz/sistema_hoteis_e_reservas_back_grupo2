52ab8e497e3929c0fb58159c9c97a8b606cfdbd5
prady001 <gabrielpac@al.insper.edu.br>
2024-10-24 16:58:20 -0300

    first commit

---
+++ sistema_reserva_hoteis/src/main/java/projeto1_grupo2/sistema_reserva_hoteis/SistemaReservaHoteisApplication.java
@@ -0,0 +1,13 @@
+package projeto1_grupo2.sistema_reserva_hoteis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaReservaHoteisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaReservaHoteisApplication.class, args);
+	}
+
+}
---
+++ sistema_reserva_hoteis/src/test/java/projeto1_grupo2/sistema_reserva_hoteis/SistemaReservaHoteisApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto1_grupo2.sistema_reserva_hoteis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaReservaHoteisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
