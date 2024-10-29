c9781eb31e165ab01bd8c5a405b0e8293a50bd4a
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2024-10-29 16:13:36 -0300

    Concertando os nomes dos DTO´s do usuario

--- sistema_reserva_hoteis/src/main/java/projeto1_grupo2/sistema_reserva_hoteis/usuario/UsuarioDTO.java
+++ sistema_reserva_hoteis/src/main/java/projeto1_grupo2/sistema_reserva_hoteis/usuario/UsuarioDTO.java
@@ -1,10 +1,10 @@
 package projeto1_grupo2.sistema_reserva_hoteis.usuario;
 
 // DTO para retornar os dados do usuário
-record RetornaUsuarioDTO(String id, String nome, String endereco, String email, String telefone) {}
+record RetornarUsuarioDTO(String id, String nome, String endereco, String email, String telefone) {}
 
 // DTO para cadastro de um novo usuário
-record CadastraUsuarioDTO(String nome, String endereco, String email, String telefone) {}
+record CadastrarUsuarioDTO(String nome, String endereco, String email, String telefone) {}
 
 // DTO para editar os dados do usuário (atualização parcial ou completa)
-record EditaUsuarioDTO(String nome, String endereco, String email, String telefone) {}
+record EditarUsuarioDTO(String nome, String endereco, String email, String telefone) {}
